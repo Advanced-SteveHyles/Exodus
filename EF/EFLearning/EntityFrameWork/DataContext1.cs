39e2ba32188cdb8babca9b9638c7e791c4baa316
Steven Hyles <steven.hyles@advancedcomputersoftware.com>
2016-11-18 10:43:04 +0000

    New EF learning exercise

---
+++ EF/EFLearning/EntityFrameWork/DataContext1.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameWork
+{
+    public class DataContext1 : DbContext
+    {
+        public DataContext1() : base("DataContext")
+        {
+        }
+    
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ EF/EFLearning/Xunit/BuildReal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFrameWork;
+
+//https://www.asp.net/mvc/overview/getting-started/getting-started-with-ef-using-mvc/creating-an-entity-framework-data-model-for-an-asp-net-mvc-application
+
+namespace Xunit
+{
+    public class BuildReal
+    {
+        public DataContext1 DataContext { get; set; }
+
+        public BuildReal()
+        {
+            this.DataContext = new EntityFrameWork.DataContext1();
+        }
+
+        [Fact]
+        public void BuildMe()
+        {
+            
+           Assert.True(true); 
+        }
+
+    }
+}
